1ca88ebdf4c6325167af19f0a1a083ef14e34149
Jongwan Kim <figurekim317@gmail.com>
2023-04-05 08:55:19 +0900

    Update nvinfo.py

--- utils/nvinfo.py
+++ utils/nvinfo.py
@@ -1,4 +1,7 @@
-nvidia-smi for each gpu in a list.
+nvidia-smiined from of stats m
+def gpu_info() -> list:
+    """
+    Returns a dictionary  for each gpu in a list.
     Adapted from nvgpu: https://pypi.org/project/nvgpu/, but mine has more info.
     """
     gpus = [line for line in _run_cmd(['nvidia-smi', '-L']) if line]
