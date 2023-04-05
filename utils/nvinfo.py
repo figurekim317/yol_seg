05309fc4804e8d3d04c6e0818693418758788cde
Jongwan Kim <figurekim317@gmail.com>
2023-04-05 09:12:48 +0900

    Update nvinfo.py

--- utils/nvinfo.py
+++ utils/nvinfo.py
@@ -1,7 +1,4 @@
-
-    Adapted from nvgpu: https://pypi.org/project/nvgpu/, but mine has more info.
-    """
-    gpus = [line for line in _run_cmd(['nvidia-smi', '-L']) if line]
+ in _run_cmd(['nvidia-smi', '-L']) if line]
     gpu_infos = [re.match('GPU ([0-9]+): ([^(]+) \(UUID: ([^)]+)\)', gpu).groups() for gpu in gpus]
     gpu_infos = [dict(zip(['idx', 'name', 'uuid'], info)) for info in gpu_infos]
     gpu_count = len(gpus)
